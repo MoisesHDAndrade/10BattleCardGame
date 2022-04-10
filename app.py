6db996ce185ee8a6e97f572e98c887f003dd843a
Moises Andrade <lawliex@gmail.com>
2022-04-10 16:03:37 +1200

    commit in bunch

---
+++ app.py
@@ -0,0 +1,54 @@
+"""
+Cards.json generator based on images on 'Cards' folder
+
+"""
+
+import os
+import json
+
+base = "/Cards/"
+cards_list = []
+cards_json = None
+
+
+def check_value(card):
+    # Return a value to the card dictionary based on card values
+    value = 0
+    splitted = card.split('.') 
+    # Split the values of card by '.' which returns the name of card image in the first position (0) and the extension in the second position (1)
+    # If the name which it is in the first position of the list ends with below cases return the value with that value
+    if splitted[0].endswith("A"):
+        return 1
+    if splitted[0].endswith("J"):
+        return 11
+    if splitted[0].endswith("Q"):
+        return 12
+    if splitted[0].endswith("K"):
+        return 13
+    if splitted[0].endswith("10"):
+        return 10
+    if "cardJoker" in splitted[0]:
+        return 1
+    else:
+        # Return the as value the number of the card if not of cases above do not match
+        return splitted[0][-1]
+
+
+if __name__ == '__main__':
+    for root, folder, files in os.walk("./Cards"): # return a list of files inside the 'cards' folder
+        files.sort() #shuffle the files before append in card_list
+        value = 0
+        for card in files:
+            if not "cardBack" in card: # if card back in the list the value will be set to 0
+                value = check_value(card)
+            # append inside the card_list the values of card name, card img path and the value the was set in the check_value function
+            cards_list.append(
+                {'card':card.split(".")[0], 'img':f'{os.path.join(base, card)}','value':int(value)}
+            )
+            value = 0 #return value to 0 in case of any card back appear in the list
+
+    cards_json = json.dumps(cards_list, indent=4) # transform the dictionary in JSON
+
+    with open('cards.json', 'w') as f:
+        # transform the JSON variable in a JSON file
+        f.write(cards_json)
---
+++ main.js
@@ -0,0 +1,531 @@
+const cardsArray = [
+    {
+        "card": "cardBack_blue1",
+        "img": "/Cards/cardBack_blue1.png",
+        "value": 0
+    },
+    {
+        "card": "cardBack_blue2",
+        "img": "/Cards/cardBack_blue2.png",
+        "value": 0
+    },
+    {
+        "card": "cardBack_blue3",
+        "img": "/Cards/cardBack_blue3.png",
+        "value": 0
+    },
+    {
+        "card": "cardBack_blue4",
+        "img": "/Cards/cardBack_blue4.png",
+        "value": 0
+    },
+    {
+        "card": "cardBack_blue5",
+        "img": "/Cards/cardBack_blue5.png",
+        "value": 0
+    },
+    {
+        "card": "cardBack_green1",
+        "img": "/Cards/cardBack_green1.png",
+        "value": 0
+    },
+    {
+        "card": "cardBack_green2",
+        "img": "/Cards/cardBack_green2.png",
+        "value": 0
+    },
+    {
+        "card": "cardBack_green3",
+        "img": "/Cards/cardBack_green3.png",
+        "value": 0
+    },
+    {
+        "card": "cardBack_green4",
+        "img": "/Cards/cardBack_green4.png",
+        "value": 0
+    },
+    {
+        "card": "cardBack_green5",
+        "img": "/Cards/cardBack_green5.png",
+        "value": 0
+    },
+    {
+        "card": "cardBack_red1",
+        "img": "/Cards/cardBack_red1.png",
+        "value": 0
+    },
+    {
+        "card": "cardBack_red2",
+        "img": "/Cards/cardBack_red2.png",
+        "value": 0
+    },
+    {
+        "card": "cardBack_red3",
+        "img": "/Cards/cardBack_red3.png",
+        "value": 0
+    },
+    {
+        "card": "cardBack_red4",
+        "img": "/Cards/cardBack_red4.png",
+        "value": 0
+    },
+    {
+        "card": "cardBack_red5",
+        "img": "/Cards/cardBack_red5.png",
+        "value": 0
+    },
+    {
+        "card": "cardClubs10",
+        "img": "/Cards/cardClubs10.png",
+        "value": 10
+    },
+    {
+        "card": "cardClubs2",
+        "img": "/Cards/cardClubs2.png",
+        "value": 2
+    },
+    {
+        "card": "cardClubs3",
+        "img": "/Cards/cardClubs3.png",
+        "value": 3
+    },
+    {
+        "card": "cardClubs4",
+        "img": "/Cards/cardClubs4.png",
+        "value": 4
+    },
+    {
+        "card": "cardClubs5",
+        "img": "/Cards/cardClubs5.png",
+        "value": 5
+    },
+    {
+        "card": "cardClubs6",
+        "img": "/Cards/cardClubs6.png",
+        "value": 6
+    },
+    {
+        "card": "cardClubs7",
+        "img": "/Cards/cardClubs7.png",
+        "value": 7
+    },
+    {
+        "card": "cardClubs8",
+        "img": "/Cards/cardClubs8.png",
+        "value": 8
+    },
+    {
+        "card": "cardClubs9",
+        "img": "/Cards/cardClubs9.png",
+        "value": 9
+    },
+    {
+        "card": "cardClubsA",
+        "img": "/Cards/cardClubsA.png",
+        "value": 1
+    },
+    {
+        "card": "cardClubsJ",
+        "img": "/Cards/cardClubsJ.png",
+        "value": 11
+    },
+    {
+        "card": "cardClubsK",
+        "img": "/Cards/cardClubsK.png",
+        "value": 13
+    },
+    {
+        "card": "cardClubsQ",
+        "img": "/Cards/cardClubsQ.png",
+        "value": 12
+    },
+    {
+        "card": "cardDiamonds10",
+        "img": "/Cards/cardDiamonds10.png",
+        "value": 10
+    },
+    {
+        "card": "cardDiamonds2",
+        "img": "/Cards/cardDiamonds2.png",
+        "value": 2
+    },
+    {
+        "card": "cardDiamonds3",
+        "img": "/Cards/cardDiamonds3.png",
+        "value": 3
+    },
+    {
+        "card": "cardDiamonds4",
+        "img": "/Cards/cardDiamonds4.png",
+        "value": 4
+    },
+    {
+        "card": "cardDiamonds5",
+        "img": "/Cards/cardDiamonds5.png",
+        "value": 5
+    },
+    {
+        "card": "cardDiamonds6",
+        "img": "/Cards/cardDiamonds6.png",
+        "value": 6
+    },
+    {
+        "card": "cardDiamonds7",
+        "img": "/Cards/cardDiamonds7.png",
+        "value": 7
+    },
+    {
+        "card": "cardDiamonds8",
+        "img": "/Cards/cardDiamonds8.png",
+        "value": 8
+    },
+    {
+        "card": "cardDiamonds9",
+        "img": "/Cards/cardDiamonds9.png",
+        "value": 9
+    },
+    {
+        "card": "cardDiamondsA",
+        "img": "/Cards/cardDiamondsA.png",
+        "value": 1
+    },
+    {
+        "card": "cardDiamondsJ",
+        "img": "/Cards/cardDiamondsJ.png",
+        "value": 11
+    },
+    {
+        "card": "cardDiamondsK",
+        "img": "/Cards/cardDiamondsK.png",
+        "value": 13
+    },
+    {
+        "card": "cardDiamondsQ",
+        "img": "/Cards/cardDiamondsQ.png",
+        "value": 12
+    },
+    {
+        "card": "cardHearts10",
+        "img": "/Cards/cardHearts10.png",
+        "value": 10
+    },
+    {
+        "card": "cardHearts2",
+        "img": "/Cards/cardHearts2.png",
+        "value": 2
+    },
+    {
+        "card": "cardHearts3",
+        "img": "/Cards/cardHearts3.png",
+        "value": 3
+    },
+    {
+        "card": "cardHearts4",
+        "img": "/Cards/cardHearts4.png",
+        "value": 4
+    },
+    {
+        "card": "cardHearts5",
+        "img": "/Cards/cardHearts5.png",
+        "value": 5
+    },
+    {
+        "card": "cardHearts6",
+        "img": "/Cards/cardHearts6.png",
+        "value": 6
+    },
+    {
+        "card": "cardHearts7",
+        "img": "/Cards/cardHearts7.png",
+        "value": 7
+    },
+    {
+        "card": "cardHearts8",
+        "img": "/Cards/cardHearts8.png",
+        "value": 8
+    },
+    {
+        "card": "cardHearts9",
+        "img": "/Cards/cardHearts9.png",
+        "value": 9
+    },
+    {
+        "card": "cardHeartsA",
+        "img": "/Cards/cardHeartsA.png",
+        "value": 1
+    },
+    {
+        "card": "cardHeartsJ",
+        "img": "/Cards/cardHeartsJ.png",
+        "value": 11
+    },
+    {
+        "card": "cardHeartsK",
+        "img": "/Cards/cardHeartsK.png",
+        "value": 13
+    },
+    {
+        "card": "cardHeartsQ",
+        "img": "/Cards/cardHeartsQ.png",
+        "value": 12
+    },
+    {
+        "card": "cardJoker",
+        "img": "/Cards/cardJoker.png",
+        "value": 1
+    },
+    {
+        "card": "cardSpades10",
+        "img": "/Cards/cardSpades10.png",
+        "value": 10
+    },
+    {
+        "card": "cardSpades2",
+        "img": "/Cards/cardSpades2.png",
+        "value": 2
+    },
+    {
+        "card": "cardSpades3",
+        "img": "/Cards/cardSpades3.png",
+        "value": 3
+    },
+    {
+        "card": "cardSpades4",
+        "img": "/Cards/cardSpades4.png",
+        "value": 4
+    },
+    {
+        "card": "cardSpades5",
+        "img": "/Cards/cardSpades5.png",
+        "value": 5
+    },
+    {
+        "card": "cardSpades6",
+        "img": "/Cards/cardSpades6.png",
+        "value": 6
+    },
+    {
+        "card": "cardSpades7",
+        "img": "/Cards/cardSpades7.png",
+        "value": 7
+    },
+    {
+        "card": "cardSpades8",
+        "img": "/Cards/cardSpades8.png",
+        "value": 8
+    },
+    {
+        "card": "cardSpades9",
+        "img": "/Cards/cardSpades9.png",
+        "value": 9
+    },
+    {
+        "card": "cardSpadesA",
+        "img": "/Cards/cardSpadesA.png",
+        "value": 1
+    },
+    {
+        "card": "cardSpadesJ",
+        "img": "/Cards/cardSpadesJ.png",
+        "value": 11
+    },
+    {
+        "card": "cardSpadesK",
+        "img": "/Cards/cardSpadesK.png",
+        "value": 13
+    },
+    {
+        "card": "cardSpadesQ",
+        "img": "/Cards/cardSpadesQ.png",
+        "value": 12
+    }
+]
+
+// cardsArray it is the code generated in python
+
+const cardBack = [
+    {
+        "card": "cardBack_red3",
+        "img": "/Cards/cardBack_red3.png",
+        "value": 0
+    },
+]
+
+// This will define the back image of our cards
+
+cardsArray.sort(() => 0.5 - Math.random()) // Shuffle the cardArrays variable
+  const app = Vue.createApp({
+    data() {
+      return {
+        player_one_cards:[],
+        player_two_cards:[],
+        available_cards: [],
+        cardBattle:[],
+        cardBack: cardBack[0],
+        draw:false,
+        clickCard:true,
+        gameOverMessage:"",
+        playing: true
+      }
+    },
+    methods:{
+        initiateCards(){
+            let available_cards_local = cardsArray
+            while (this.player_one_cards.length < 10){
+                this.available_cards = available_cards_local
+                let rand = Math.round(Math.random() * available_cards_local.length - 1)
+                let poped = available_cards_local[rand]
+                if(poped && poped.value > 0 ){
+                    let poped_local = this.available_cards.pop(poped)
+                    if(poped_local.value > 0){
+                        
+                        this.player_one_cards.push(poped_local)
+                    }
+                }
+                
+            }
+           
+            while (this.player_two_cards.length < 10){
+                this.available_cards = available_cards_local
+                let rand = Math.round(Math.random() * available_cards_local.length - 1)
+                let poped = available_cards_local[rand]
+                if(poped && poped.value > 0 ){
+                    let poped_local = this.available_cards.pop(poped)
+                    if(poped_local.value > 0){
+                        this.player_two_cards.push(poped_local)
+                    }     
+                }
+            } 
+        },
+
+        playSound(src){
+            const audio = new Audio()
+            audio.src = src
+            audio.play()
+        },
+
+        intervalClick(){
+            this.clickCard = false
+            setTimeout(()=>{
+                this.clickCard = true
+            }, 1500)
+        },
+
+        initiateBattle(card){
+            if(!this.playing){
+                return
+            }
+            if(this.clickCard){
+                let playerOne = this.player_one_cards.splice(this.player_one_cards.indexOf(card),1)
+
+                this.cardBattle.push(playerOne[0])
+                this.playSound('./sound/cardPlace1.ogg')
+                
+                setTimeout(()=>{
+                    let rand = Math.round(Math.random() * this.player_two_cards.length - 1)
+                    let playerTwo = this.player_two_cards.splice(this.player_two_cards.indexOf(this.player_two_cards[rand]),1)
+                    this.cardBattle.push(playerTwo[0])
+                    this.playSound('./sound/cardPlace1.ogg')
+                    setTimeout(()=>{
+                        
+                        this.compareCards()
+                    },1000)
+                },1000)
+            }
+            this.intervalClick()
+            
+        },
+        compareCards(){
+            if(!this.draw){
+                console.log(this.cardBattle.length)
+                if(this.cardBattle.length == 2){
+                    if(this.cardBattle[0].value > this.cardBattle[1].value){
+                        for(var item of this.cardBattle){
+                            this.player_two_cards.push(item)
+                            this.player_two_cards.sort(() => 0.5 - Math.random())
+                            this.cardBattle = []
+                            this.draw = false
+                        }
+                    }
+                    else if(this.cardBattle[0].value < this.cardBattle[1].value){
+                        for(var item of this.cardBattle){
+                            this.player_one_cards.push(item)
+                            this.player_one_cards.sort(() => 0.5 - Math.random())
+                            this.cardBattle = []
+                            this.draw = false
+                        }
+                    }
+                    else{
+                        this.draw = true
+                    }
+                }
+            }
+            else{
+                
+                let counter = 4
+                if(this.cardBattle.length == counter){
+                    if(this.cardBattle[counter-2].value > this.cardBattle[counter-1].value){
+                        for(var item of this.cardBattle){
+                            this.player_two_cards.push(item)
+                            this.cardBattle = []
+                            this.draw = false
+                        }
+                    }
+                    else if(this.cardBattle[counter-2].value < this.cardBattle[counter-1].value){
+                        for(var item of this.cardBattle){
+                            this.player_one_cards.push(item)
+                            this.cardBattle = []
+                            this.draw = false
+                        }
+                    }
+                    else if(this.cardBattle[counter-2].value == this.cardBattle[counter-1].value){
+                        counter += 2
+                        return
+                    }
+                    else{
+                        this.draw = false
+                        counter = 4
+                    }
+
+                }
+
+            }
+            this.playSound('./sound/cardSlide1.ogg')
+            this.checkVictory()
+        },
+        checkVictory(){
+            if(this.player_one_cards.length <=0){
+                this.gameOverMessage = "Player One Won"
+                this.openModal()
+                this.playing = false
+            }
+            if(this.player_two_cards.length <=0){
+                this.gameOverMessage = "Player Two Won"
+                this.openModal()
+                this.playing = false
+            }
+            
+        },
+        openModal(){
+            var endgame  = new bootstrap.Modal(document.getElementById('endgame'))
+            endgame.show()
+            
+        },
+        resetGame(){
+            this.player_one_cards = [],
+            this.player_two_cards = [],
+            this.available_cards =  [],
+            this.cardBattle = [],
+            this.draw = false,
+            this.clickCard = true,
+            this.gameOverMessage = "",
+            this.playing =  true
+            this.initiateCards()
+        }
+    },
+    mounted(){
+        this.initiateCards()
+        
+    }
+
+  })
